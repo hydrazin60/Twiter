96afdf3e08b358cd35ff20a0af215464a0326e0f
hydrazin60 <sureshkhdk45@gmail.com>
2024-06-26 06:30:32 -0700

    Login Funcenlity create

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/User.Model.js";
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 export const SignUp = async (req, res) => {
   try {
     const { name, username, email, password } = req.body;
@@ -34,6 +34,57 @@ export const SignUp = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
+    return res.status(500).json({
+      message: "Server Error",
+      success: false,
+    });
+  }
+};
+// login
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+    const RegisterUser = await User.findOne({ email });
+    if (!RegisterUser) {
+      return res.status(404).json({
+        message: "User does not exist",
+        success: false,
+      });
+    }
+    const ValidPassword = await bcryptjs.compare(
+      password,
+      RegisterUser.password
+    );
+    if (!ValidPassword) {
+      return res.status(401).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+    const tokenData = {
+      userId: RegisterUser._id,
+    };
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
+      expiresIn: "360d",
+    });
+    return res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: 360 * 24 * 60 * 60 * 1000,
+      })
+      .json({
+        message: `Welcome ${RegisterUser.name} you are successfully login `,
+        success: true,
+      });
+  } catch (err) {
+    console.error(err);
     return res.status(500).json({
       message: "Server Error",
       success: false,
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { SignUp } from "../controllers/auth.controller.js";
+import { Login, SignUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.route("/register").post(SignUp);
-
+router.route("/login").post(Login)
 export default router;
