a1ccf288ac36ec50eb9633598e3ac0c7867547c5
hydrazin60 <sureshkhdk45@gmail.com>
2024-12-01 18:51:28 +0545

    reCreate twiter project Frontend + Backend

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,14 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
-import database from "./DB/database.js";
-import cookieParser from "cookie-parser";
-import router from "./routes/auth.route.js";
-import TweetRoute from "./routes/tweet.route.js";
-
+ 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-database();
+ 
 
-//middlewares
-app.use(express.json());
-app.use(cookieParser());
-
-// Api
-app.use("/api/v1/user", router);
-app.use("/api/v1/tweet" ,  TweetRoute)
-
+ 
+ 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LeftSidebar from "./components/LeftSidebar";
-import Home from "./components/Home"; // Update with your correct path
-import Profile from "./pages/Profile";
-import RightSidebar from "./components/RightSidebar";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
 
 export default function App() {
-  return (
-    <div className="text-3xl bg-black text-white w-full h-[100%]">
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="flex justify-between">
-                <LeftSidebar />
-                <Home />
-                <RightSidebar />
-              </div>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <div className="flex justify-between">
-                <LeftSidebar />
-                <Profile />
-                <RightSidebar />
-              </div>
-            }
-          />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  return <div>App</div>;
 }
